19f65957f23023dbdcdfbb9b9d56173f885f2821
haneeyahya <haneenjawarneh@gmail.com>
2019-05-24 05:13:37 +0300

    Updates

---
+++ precache-manifest.6b15930a70399862d68b8349876c371f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ded4da0fe71cbdcab24f",
+    "url": "/hello/static/js/runtime~main.f3b3c316.js"
+  },
+  {
+    "revision": "69f271ca5c74181b3eef",
+    "url": "/hello/static/js/main.a4ad88f2.chunk.js"
+  },
+  {
+    "revision": "0bcf5a1fb50d05f2db47",
+    "url": "/hello/static/js/2.a936f4c1.chunk.js"
+  },
+  {
+    "revision": "69f271ca5c74181b3eef",
+    "url": "/hello/static/css/main.95972601.chunk.css"
+  },
+  {
+    "revision": "126327fb8440fad03971826ac9f26d7a",
+    "url": "/hello/index.html"
+  }
+];
